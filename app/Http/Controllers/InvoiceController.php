744a6a9034ec25e6de217f918fd5a1a84a7993dd
Harsha Sandakelum <mmharshasandakelum@gmail.com>
2024-12-23 23:20:21 +0530

    make api for get all invoices and write async function to get data

--- app/Http/Controllers/InvoiceController.php
+++ app/Http/Controllers/InvoiceController.php
@@ -2,9 +2,33 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Invoice;
 use Illuminate\Http\Request;
 
 class InvoiceController extends Controller
 {
-    //
+    public function get_all_invoice(){
+        $invoices = Invoice::with('customer')->orderBy('id', 'ASC')->get();
+
+        return response()->json([
+            'invoices' => $invoices
+        ], 200);
+    }
+    
+    public function search_invoice(Request $request){
+        $search = $request->s;
+
+        if($search != null){
+            $invoices = Invoice::with('customer')
+            ->where('id', 'LIKE', "%$search%")
+            ->get();
+
+            return response()->json([
+                'invoices' => $invoices
+            ], 200);
+        } else {
+            return $this->get_all_invoice();
+        }
+       
+    }
 }
--- resources/js/component/invoice/index.vue
+++ resources/js/component/invoice/index.vue
@@ -1,3 +1,28 @@
+<script setup>
+import axios from "axios";
+import { onMounted, ref } from "vue";
+
+let invoices = ref([]);
+let searchInvoice = ref([]);
+
+onMounted(async () => {
+    getInvoices();
+});
+
+const getInvoices = async () => {
+    let response = await axios.get("/api/get_all_invoice");
+    console.log("response :", response);
+    invoices.value = response.data.invoices;
+};
+
+const search = async () => {
+    let response = await axios.get(
+        "/api/search_invoice?s=" + searchInvoice.value
+    );
+    // console.log("response", response);
+    invoices.value = response.data.invoices;
+};
+</script>
 <template>
     <div class="container">
         <div class="invoices">
@@ -43,6 +68,8 @@
                             class="table--search--input"
                             type="text"
                             placeholder="Search invoice"
+                            v-model="searchInvoice"
+                            @keyup="search()"
                         />
                     </div>
                 </div>
@@ -57,13 +84,24 @@
                 </div>
 
                 <!-- item 1 -->
-                <div class="table--items">
-                    <a href="#" class="table--items--transactionId">#093654</a>
-                    <p>Jan 18, 9:31am</p>
-                    <p>#093654</p>
-                    <p>Jonathan Yu</p>
-                    <p>Jan 18, 9:31am</p>
-                    <p>$ 16,943</p>
+                <div
+                    class="table--items"
+                    v-for="item in invoices"
+                    :key="item.id"
+                    v-if="invoices.length > 0"
+                >
+                    <a href="#" class="table--items--transactionId"
+                        >#{{ item.id }}</a
+                    >
+                    <p>{{ item.date }}</p>
+                    <p>#{{ item.number }}</p>
+                    <p v-if="item.customer">{{ item.customer.first_name }}</p>
+                    <p v-else></p>
+                    <p>{{ item.due_date }}</p>
+                    <p>Rs {{ item.total }}</p>
+                </div>
+                <div class="table--items" v-else>
+                    <p>Invoices Not Found</p>
                 </div>
             </div>
         </div>
