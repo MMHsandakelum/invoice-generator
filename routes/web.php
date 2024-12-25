72abb9dbe6d10377055b404e28af8d78e6dcb581
Harsha Sandakelum <mmharshasandakelum@gmail.com>
2024-12-26 04:08:36 +0530

    test

--- routes/web.php
+++ routes/web.php
@@ -13,10 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
-
-Route::get('/{pathMatch}', function(){
+Route::get('/', function () {
     return view('welcome');
-})->where('pathMatch',".*");
+});
+
+// Route::get('/{pathMatch}', function(){
+//     return view('welcome');
+// })->where('pathMatch',".*");
